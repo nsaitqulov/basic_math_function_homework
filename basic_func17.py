2036cf78f6aee93be74fc361ff5e3d4a1a5a791c
Jalilov <javohirjalilov777@gmail.com>
2022-11-17 11:07:50 +0500

    done

---
+++ basic_func17.py
@@ -0,0 +1,10 @@
+def main(a):
+    '''Assign the value pi to the parametr "a" and return it.
+    
+    Args:
+        a (float): a number
+        
+    Returns:
+        float: the value of pi.
+    '''
+    return
